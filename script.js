439646d8cdb807f3f2a9e974ee6669420e8c6de6
farukhkhanrajput0286-eng <farukhkhanrajput0286@gmail.com>
2026-02-24 12:17:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("introduction to variables in js");
+// // var variable:
+// // var name = "Farukh";
+// // console.log(name);
+  
+//  // const variable:
+//  var name;
+//  console.log(name);
+         
+    // data types i js:
+    let name =" farukh";
+    let age = 20;
+    let isstudent = true;
+    let score;
+    let car = null;
+    let person = {name:"farukh", age:20,isstudent:true,score:85,car:null}
+     console.log(person);
