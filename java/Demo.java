e3f6a7d8c881634983d7b4cbeb65c321128fc60c
farukhkhanrajput0286-eng <farukhkhanrajput0286@gmail.com>
2026-06-18 22:45:26 +0530

    Java

---
+++ java/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    String name;
+    int age;
+    public Demo(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args){
+        System.out.println("Name: "+ name ", age: " + age);
+    }
+}
